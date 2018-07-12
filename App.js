148413d795af613afc85ce64f08ecbeb6f1d237f
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2018-07-12 19:45:16 +0200

    Add GIPHY MCVE
    giphy-js-sdk-core v1.0.3 installed.

--- App.js
+++ App.js
@@ -7,23 +7,40 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import GphApiClient from 'giphy-js-sdk-core'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const client = GphApiClient('')
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: false };
+  }
+
+  trendingGifs = () => {
+    this.setState({ isLoading: true })
+    client.trending("gifs", {})
+      .then((response) => {
+        this.setState({ isLoading: false })
+        alert('success')
+      })
+      .catch((err) => {
+        alert('error')
+        this.setState({ isLoading: false })
+      })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Giphy Core SDK for JS Bug #22</Text>
+        <Button
+          onPress={this.trendingGifs}
+          disabled={this.state.isLoading}
+          title="Fetch trending GIFs"
+        />
       </View>
     );
   }
@@ -34,16 +51,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
